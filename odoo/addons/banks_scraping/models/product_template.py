e923c3d1d50b3b7e20101a04e26cadcd2dd0dd37
Chris Busillo <info@shinycomputers.com>
2023-07-04 01:16:25 -0400

    update to new mpn field

--- odoo/addons/banks_scraping/models/product_template.py
+++ odoo/addons/banks_scraping/models/product_template.py
@@ -29,19 +29,19 @@ class ProductTemplate(models.Model):
         "product.scraper", compute="_compute_product_scraper_id", store=True, readonly=False, string="Product Scraper"
     )
     product_scraper_html = fields.Text(related="product_scraper_id.source_url_html", string="Product Scraper HTML", readonly=True)
-    combined_description = fields.Html(compute="_compute_combined_description", readonly=True)
+    combined_description = fields.Html(compute="_compute_combined_description", readonly=False)
 
     @api.depends("description_sale", "product_scraper_html")
     def _compute_combined_description(self):
         for record in self:
             record.combined_description = f"{record.description_sale or ''} {record.product_scraper_html or ''}"
 
-    @api.depends("default_code")
+    @api.depends("mpn")
     def _compute_product_scraper_id(self):
         for record in self:
-            sku = (record.default_code or "").split(" ")[0]
-            if sku:
-                product_scraper = self.env["product.scraper"].search([("sku", "=", sku)], limit=1)
+            mpn = (record.mpn or "").split(" ")[0]
+            if mpn:
+                product_scraper = self.env["product.scraper"].search([("sku", "=", mpn)], limit=1)
                 record.product_scraper_id = product_scraper
 
     @api.constrains("default_code")
