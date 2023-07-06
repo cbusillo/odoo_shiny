be655446d70624bd4c57b58cd489d41ffb928767
Chris Busillo <info@shinycomputers.com>
2023-07-06 14:55:41 -0400

    reorder manifest

--- odoo/addons/product_connect/__manifest__.py
+++ odoo/addons/product_connect/__manifest__.py
@@ -14,9 +14,9 @@
         "data/res_config_data.xml",
         "security/ir.model.access.csv",
         "views/printnode_interface_views.xml",
+        "views/product_import_views.xml",
         "views/product_import_wizard.xml",
         "views/product_scraper_wizard.xml",  # Needs to be before product_template_views.xml to allow button for action
-        "views/product_import_views.xml",
         "views/product_product_views.xml",
         "views/product_scraper_views.xml",
         "views/product_template_views.xml",
