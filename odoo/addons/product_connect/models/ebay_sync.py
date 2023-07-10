b37a82a6b5f0a8392ff662198f22168f46c045f7
Chris Busillo <info@shinycomputers.com>
2023-07-10 13:26:28 -0400

    inital working read

--- odoo/addons/product_connect/models/ebay_sync.py
+++ odoo/addons/product_connect/models/ebay_sync.py
@@ -1,71 +1,29 @@
 import os
+import logging
+from pathlib import Path
 
-from ebay_rest import API, DateTime, Error, Reference
+from ebay_rest import API, Error
 
-from odoo import models, fields, api
+from odoo import models, api
 
 
 class EbaySync(models.AbstractModel):
     _name = "ebay.sync"
     _description = "eBay Sync"
 
-    # @api.model
-    # def _setup_complete(self):
-    #     super(EbaySync, self)._setup_complete()
-    # application = {
-    #     "app_id": "placeholder-placeholder-PRD-placeholder-placeholder",
-    #     "cert_id": "PRD-placeholder-placeholder-placeholder-placeholder-placeholder",
-    #     "dev_id": "placeholder-placeholder-placeholder-placeholder-placeholder",
-    #     "redirect_uri": "placeholder-placeholder-placeholder-placeholder",
-    # }
+    @api.model
+    def _setup_complete(self):
+        super(EbaySync, self)._setup_complete()
+        try:
+            ebay_api = API(application="sandbox_1", user="sandbox_1", path=Path.home() / ".shiny", header="US")
+        except Error as error:
+            logging.fatal(error)
+            os._exit(1)  # pylint: disable=protected-access
 
-    # user = {
-    #     "email_or_username": "<production-username>",
-    #     "password": "<production-user-password>",
-    #     "scopes": [
-    #         "https://api.ebay.com/oauth/api_scope",
-    #         "https://api.ebay.com/oauth/api_scope/sell.inventory",
-    #         "https://api.ebay.com/oauth/api_scope/sell.marketing",
-    #         "https://api.ebay.com/oauth/api_scope/sell.account",
-    #         "https://api.ebay.com/oauth/api_scope/sell.fulfillment",
-    #     ],
-    #     "refresh_token": "production-refresh_token",
-    #     "refresh_token_expiry": "production-token_expiry",
-    # }
+        for record in ebay_api.buy_browse_search(q="iphone", sort="price", limit=10):
+            if "record" not in record:
+                continue
+            logging.info(record["record"].get("title", "No title"))
 
-    # header = {
-    #     "accept_language": "en-US",
-    #     "affiliate_campaign_id": "",
-    #     "affiliate_reference_id": "",
-    #     "content_language": "en-US",
-    #     "country": "US",
-    #     "currency": "USD",
-    #     "device_id": "",
-    #     "marketplace_id": "EBAY_US",
-    #     "zip": "",
-    # }
-
-    # key_pair = {
-    #     "private_key": "placeholder-placeholder-placeholder-placeholder-placeholder-plac",
-    #     "signing_key_id": "placeholder-placeholder-placeholder-",
-    # }
-
-    # try:
-    #     ebay_api = API(application=application, user=user, header=header)
-    # except Error as error:
-    #     print(f"Error {error.number} is {error.reason}  {error.detail}.\n")
-    # else:
-    #     try:
-    #         print("The five least expensive iPhone things now for sale on-eBay:")
-    #         for record in ebay_api.buy_browse_search(q="iPhone", sort="price", limit=5):
-    #             if "record" not in record:
-    #                 pass  # TODO Refer to non-records, they contain optimization information.
-    #             else:
-    #                 item = record["record"]
-    #                 print(f"item id: {item['item_id']} {item['item_web_url']}")
-    #     except Error as error:
-    #         print(f"Error {error.number} is {error.reason} {error.detail}.\n")
-    #     else:
-    #         pass
-
-    # os._exit(0)
+        os._exit(1)  # pylint: disable=protected-access
+        return
