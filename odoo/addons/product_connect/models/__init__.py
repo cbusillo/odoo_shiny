28d30e3349e087f199dd8fa7f8b01b04958cc3cd
Chris Busillo <info@shinycomputers.com>
2023-07-10 14:26:00 -0400

    remove from wrong branch

--- odoo/addons/product_connect/models/__init__.py
+++ odoo/addons/product_connect/models/__init__.py
@@ -1,4 +1,3 @@
 from . import printnode_interface
 from . import product_import, product_scraper, product_product, product_manufacturer, product_template
 from . import shopify_sync
-from . import ebay_sync
