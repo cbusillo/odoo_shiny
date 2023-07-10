8eeed98d1559b55931b99d5122f8f62cc8466c10
Chris Busillo <info@shinycomputers.com>
2023-07-09 20:27:55 -0400

    remove mine and add multi edit

--- odoo/addons/product_connect/models/__init__.py
+++ odoo/addons/product_connect/models/__init__.py
@@ -2,4 +2,3 @@ from . import printnode_interface
 from . import product_import, product_scraper, product_product, product_manufacturer, product_template
 from . import shopify_sync
 from . import ebay_sync
-from . import ir_model
