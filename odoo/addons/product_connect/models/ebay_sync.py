5b1de3dc74b81caa40a548e928280cf60bd75454
Chris Busillo <info@shinycomputers.com>
2023-07-10 13:58:57 -0400

    setup test item pull success

--- odoo/addons/product_connect/models/ebay_sync.py
+++ odoo/addons/product_connect/models/ebay_sync.py
@@ -15,15 +15,22 @@ class EbaySync(models.AbstractModel):
     def _setup_complete(self):
         super(EbaySync, self)._setup_complete()
         try:
-            ebay_api = API(application="sandbox_1", user="sandbox_1", path=Path.home() / ".shiny", header="US")
+            ebay_api = API(application="production_1", user="production_1", path=Path.home() / ".shiny", header="US")
         except Error as error:
             logging.fatal(error)
             os._exit(1)  # pylint: disable=protected-access
+        try:
+            for record in ebay_api.buy_browse_search(q="iphone", sort="price", limit=10):
+                if "record" not in record:
+                    continue
+                logging.info(record["record"].get("title", "No title"))
 
-        for record in ebay_api.buy_browse_search(q="iphone", sort="price", limit=10):
-            if "record" not in record:
-                continue
-            logging.info(record["record"].get("title", "No title"))
-
-        os._exit(1)  # pylint: disable=protected-access
+            for record in ebay_api.sell_inventory_get_inventory_items(limit=10):
+                if "record" not in record:
+                    continue
+                logging.info(record["record"].get("product").get("title", "No title"))
+        except Error as error:
+            logging.fatal(error)
+        finally:
+            os._exit(1)  # pylint: disable=protected-access
         return
